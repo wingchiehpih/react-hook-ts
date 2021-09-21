10627a58add4f3fbe180558ce44a1f3ea836bad5
wingchiehpih <wingchiehpih@gmail.com>
2021-09-21 22:43:17 +0800

    add: useContext

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,22 @@ interface IShowResult {
   message: string;
   status: string;
 }
+
+interface IThemeProps {
+  [key: string]: { color: string; background: string };
+}
+const themes: IThemeProps = {
+  light: {
+    color: '#000',
+    background: '#eee',
+  },
+  dark: {
+    color: '#fff',
+    background: '#222',
+  },
+};
+
+export const ThemeContext = React.createContext(themes.light);
 const App: React.FC = () => {
   const [show, setShow] = useState(true);
   const positions = useMousePosition();
@@ -15,27 +31,29 @@ const App: React.FC = () => {
   const dogResult = data as IShowResult;
   return (
     <div className='App'>
-      <div>-----------第一个组件hello------------</div>
-      <Hello />
-      <div>------------第二个组件点赞---------------</div>
-      <LikeButton />
-      <button onClick={() => setShow(!show)}>切换</button>
-      <div>---------------第三个组件鼠标位置</div>
-      {show && <MouseTracker />}
-      <div>自定义hooks展示内容-----------</div>
-      <div>
-        {positions.x}，{positions.y}
-      </div>
-      <div>显示加载图----------------</div>
-      {loading ? (
-        <p>dogLoading</p>
-      ) : (
-        <img
-          style={{ width: '100px', height: '100px' }}
-          src={dogResult && dogResult.message}
-          alt=''
-        />
-      )}
+      <ThemeContext.Provider value={themes.light}>
+        <div>-----------第一个组件hello------------</div>
+        <Hello />
+        <div>------------第二个组件点赞---------------</div>
+        <LikeButton />
+        <button onClick={() => setShow(!show)}>切换</button>
+        <div>---------------第三个组件鼠标位置</div>
+        {show && <MouseTracker />}
+        <div>自定义hooks展示内容-----------</div>
+        <div>
+          {positions.x}，{positions.y}
+        </div>
+        <div>显示加载图----------------</div>
+        {loading ? (
+          <p>dogLoading</p>
+        ) : (
+          <img
+            style={{ width: '100px', height: '100px' }}
+            src={dogResult && dogResult.message}
+            alt=''
+          />
+        )}
+      </ThemeContext.Provider>
     </div>
   );
 };
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { ThemeContext } from '../App';
 interface IHelloProps {
   message?: string;
 }
 
 // react官方定义的interface:react.FC,接受一个泛型，经过interface和泛型的修饰，props被PropsWithChildren定义
 // 定义后Hello具备该接口的属性
+
 const Hello: React.FunctionComponent<IHelloProps> = props => {
-  return <div>{props.message}</div>;
+  const theme = useContext(ThemeContext);
+
+  const style = {
+    background: theme.background,
+    color: theme.color,
+  };
+  return <div style={style}>{props.message}</div>;
 };
 
 Hello.defaultProps = {
